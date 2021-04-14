8b9cd9406c2310568ba528af9cd12861040e28a9
yingspvd <supavadeeying@outlook.com>
2021-04-14 15:42:19 +0700

    Lab2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from'@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+
+  constructor() { 
+    this.products = [];
+    this.products.push({
+      name: "ส้มโอ",
+      price:990
+    });
+    this.products.push({
+      name: 'แตงโม',
+      price: 1000
+      });
+    this.products.push({
+    name: 'มะพร้าวนํ้าหอม',
+    price: 500
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
